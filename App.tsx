0073673372ab2ad86985e28b7e9800d15d6c2865
Henry Allen <hmallen99@gmail.com>
2024-08-20 15:44:50 -0400

    Use BabylonReactNative in TestApp

--- App.tsx
+++ App.tsx
@@ -2,122 +2,65 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
  * @format
  */
 
-import React from "react";
-import type { PropsWithChildren } from "react";
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from "react-native";
+import React, {
+  FunctionComponent,
+  useEffect,
+  useCallback,
+  useState,
+} from "react";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen";
+import { SafeAreaView, View, Button, ViewProps, StatusBar } from "react-native";
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import { EngineView, useEngine } from "@babylonjs/react-native";
+import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
+import { Camera } from "@babylonjs/core/Cameras/camera";
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import "@babylonjs/loaders/glTF";
 
-function Section({ children, title }: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === "dark";
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}
-      >
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}
-      >
-        {children}
-      </Text>
-    </View>
-  );
-}
+const EngineScreen: FunctionComponent<ViewProps> = (props: ViewProps) => {
+  const engine = useEngine();
+  const [camera, setCamera] = useState<Camera>();
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === "dark";
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // Taken from https://github.com/BabylonJS/BabylonReactNativeSample/blob/main/App.tsx
+  useEffect(() => {
+    if (engine) {
+      const url =
+        "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/BoxAnimated/glTF/BoxAnimated.gltf";
+      SceneLoader.LoadAsync(url, undefined, engine).then((loadScene) => {
+        loadScene.createDefaultCameraOrLight(true, undefined, true);
+        (loadScene.activeCamera as ArcRotateCamera).alpha += Math.PI;
+        (loadScene.activeCamera as ArcRotateCamera).radius = 10;
+        setCamera(loadScene.activeCamera!);
+      });
+    }
+  }, [engine]);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? "light-content" : "dark-content"}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
-      >
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}
-        >
-          <Section title="Benchmarking">Benchmarking Test App</Section>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+      <View style={props.style}>
+        <View style={{ flex: 1 }}>
+          <EngineView camera={camera} displayFrameRate={true} />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-  },
-  highlight: {
-    fontWeight: "700",
-  },
-});
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+        <EngineScreen style={{ flex: 1 }} />
+      </SafeAreaView>
+    </>
+  );
+};
 
 export default App;
