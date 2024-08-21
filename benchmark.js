ac092eb1a5520a251ee2c801530fc2a1079409a8
Henry Allen <hmallen99@gmail.com>
2024-08-21 12:05:39 -0400

    Add simple benchmarking app

---
+++ benchmark.js
@@ -0,0 +1,43 @@
+const { remote } = require("webdriverio");
+const { writeFile } = require("fs");
+
+const capabilities = {
+  platformName: "iOS",
+  "appium:automationName": "XCUITest",
+  "appium:deviceName": "iPhone 15",
+  "appium:app": "com.apple.Preferences",
+};
+
+const wdOpts = {
+  hostname: process.env.APPIUM_HOST || "localhost",
+  port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
+  logLevel: "info",
+  capabilities,
+};
+
+async function runTest() {
+  const driver = await remote(wdOpts);
+  try {
+    await driver.execute("mobile: startPerfRecord", {
+      profileName: "Time Profiler",
+      pid: "current",
+      timeout: 2000,
+    });
+    await driver.execute("mobile: scroll", { direction: "down" });
+  } finally {
+    await driver.pause(1000);
+    const output = await driver.execute("mobile: stopPerfRecord", {
+      profileName: "Time Profiler",
+    });
+
+    let buff = await Buffer.from(output, "base64");
+    await writeFile("./perfTrace.zip", buff, (err) => {
+      if (err) throw err;
+      console.log("File Written");
+    });
+
+    await driver.deleteSession();
+  }
+}
+
+runTest().catch(console.error);
