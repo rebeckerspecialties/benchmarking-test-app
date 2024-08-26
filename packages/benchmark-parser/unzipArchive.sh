1cf07a54c7f1f5d86093e80924fbf4ed37606058
Henry Allen <hmallen99@gmail.com>
2024-08-26 17:44:05 -0400

    Scope search to artifact file

--- packages/benchmark-parser/unzipArchive.sh
+++ packages/benchmark-parser/unzipArchive.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-artifactPath=$(find . -name '*Customer Artifacts.zip' | head -n 1)
+artifactPath=$(find . -name "$1/*Customer Artifacts.zip" | head -n 1)
 echo $artifactPath
-unzip "$artifactPath" -d "$1" -x *.zip
+unzip "$artifactPath" -d "$2" -x *.zip
 
-find "$1" -name '*-clock.txt' | while read clockFile; do
+find "$2" -name '*-clock.txt' | while read clockFile; do
   echo "$clockFile"
 done
