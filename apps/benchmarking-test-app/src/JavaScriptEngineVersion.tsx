0d4b351045e3678773d76d5ce9401819e2323815
Henry Allen <31718268+hmallen99@users.noreply.github.com>
2024-08-26 11:35:24 -0700

    Benchmark both JSC and Hermes engines (#23)
    * Add jsc hermes
    
    * Delete jsc config
    
    * Add script to install hermes or jsc
    
    * Update benchmark workflow
    
    * move role to secret
    
    * Renameartifact destination

---
+++ apps/benchmarking-test-app/src/JavaScriptEngineVersion.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+export const JavaScriptEngineVersion: React.FC = () => {
+  if (isHermes()) {
+    return <Text>Using Hermes</Text>;
+  }
+  return <Text>Using JavaScriptCore</Text>;
+};
+
+const isHermes = () =>
+  !!(global as unknown as { HermesInternal: null | {} }).HermesInternal;
