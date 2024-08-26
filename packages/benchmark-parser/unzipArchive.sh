0cb769fbd3e2808a281ac5c9922f3192daf3f4f3
Henry Allen <hmallen99@gmail.com>
2024-08-26 17:54:32 -0400

    Extract only txt files

--- packages/benchmark-parser/unzipArchive.sh
+++ packages/benchmark-parser/unzipArchive.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-artifactPath=$(find . -name "$1/*Customer Artifacts.zip" | head -n 1)
+artifactPath=$(find "./$1" -name "*Customer Artifacts.zip" | head -n 1)
 echo $artifactPath
-unzip "$artifactPath" -d "$2" -x *.zip
 
-find "$2" -name '*-clock.txt' | while read clockFile; do
+mkdir "$2"
+unzip "$artifactPath" -d "$2/output" -x *.zip
+
+find "$2/output" -name '*-clock.txt' | while read clockFile; do
   echo "$clockFile"
+  mv "$clockFile" "$2"
 done
+
+rm -rf "$2/output"
