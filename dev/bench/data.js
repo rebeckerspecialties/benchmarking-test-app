33665c56bb18c5873760215b67f392be54f4c1d7
Henry Allen <hmallen99@gmail.com>
2024-08-27 16:10:03 -0400

    Remove outdated spec

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,112 +1,60 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724788479938,
-  "repoUrl": "https://github.com/rebeckerspecialties/benchmarking-test-app",
-  "entries": {
+  lastUpdate: 1724788479938,
+  repoUrl: "https://github.com/rebeckerspecialties/benchmarking-test-app",
+  entries: {
     "React Native Benchmarks": [
       {
-        "commit": {
-          "author": {
-            "name": "Henry Allen",
-            "username": "hmallen99",
-            "email": "hmallen99@gmail.com"
+        commit: {
+          author: {
+            name: "Henry Allen",
+            username: "hmallen99",
+            email: "hmallen99@gmail.com",
           },
-          "committer": {
-            "name": "Henry Allen",
-            "username": "hmallen99",
-            "email": "hmallen99@gmail.com"
+          committer: {
+            name: "Henry Allen",
+            username: "hmallen99",
+            email: "hmallen99@gmail.com",
           },
-          "id": "0ded53cb0484e2b07013a1fc3c1f9b8e2950496b",
-          "message": "Fix parameters",
-          "timestamp": "2024-08-27T18:42:26Z",
-          "url": "https://github.com/rebeckerspecialties/benchmarking-test-app/commit/0ded53cb0484e2b07013a1fc3c1f9b8e2950496b"
+          id: "3dbac4cc2c8f8a8906ce1c21c957b12493223061",
+          message: "Update Benchmark name",
+          timestamp: "2024-08-27T19:27:07Z",
+          url: "https://github.com/rebeckerspecialties/benchmarking-test-app/commit/3dbac4cc2c8f8a8906ce1c21c957b12493223061",
         },
-        "date": 1724785786600,
-        "tool": "customSmallerIsBetter",
-        "benches": [
+        date: 1724788479132,
+        tool: "customSmallerIsBetter",
+        benches: [
           {
-            "name": "jsc/Apple iPhone 15 Pro/bitEcsBenchmark-clock.txt",
-            "value": 4443,
-            "unit": "clock"
+            name: "jsc/Apple iPhone 15 Pro/bitEcsBenchmarkWallClock-ms.txt",
+            value: 4538,
+            unit: "ms",
           },
           {
-            "name": "jsc/Apple iPhone 15 Pro/simpleBenchmark-clock.txt",
-            "value": 5764,
-            "unit": "clock"
+            name: "jsc/Apple iPhone 15 Pro/simpleBenchmarkWallClock-ms.txt",
+            value: 5747,
+            unit: "ms",
           },
           {
-            "name": "jsc/Apple iPhone 15 Pro/threeJsBenchmark-clock.txt",
-            "value": 688,
-            "unit": "clock"
+            name: "jsc/Apple iPhone 15 Pro/threeJsBenchmarkWallClock-ms.txt",
+            value: 674,
+            unit: "ms",
           },
           {
-            "name": "hermes/Apple iPhone 15 Pro/bitEcsBenchmark-clock.txt",
-            "value": 4242,
-            "unit": "clock"
+            name: "hermes/Apple iPhone 15 Pro/bitEcsBenchmarkWallClock-ms.txt",
+            value: 4249,
+            unit: "ms",
           },
           {
-            "name": "hermes/Apple iPhone 15 Pro/simpleBenchmark-clock.txt",
-            "value": 5732,
-            "unit": "clock"
+            name: "hermes/Apple iPhone 15 Pro/simpleBenchmarkWallClock-ms.txt",
+            value: 5732,
+            unit: "ms",
           },
           {
-            "name": "hermes/Apple iPhone 15 Pro/threeJsBenchmark-clock.txt",
-            "value": 1214,
-            "unit": "clock"
-          }
-        ]
+            name: "hermes/Apple iPhone 15 Pro/threeJsBenchmarkWallClock-ms.txt",
+            value: 1182,
+            unit: "ms",
+          },
+        ],
       },
-      {
-        "commit": {
-          "author": {
-            "name": "Henry Allen",
-            "username": "hmallen99",
-            "email": "hmallen99@gmail.com"
-          },
-          "committer": {
-            "name": "Henry Allen",
-            "username": "hmallen99",
-            "email": "hmallen99@gmail.com"
-          },
-          "id": "3dbac4cc2c8f8a8906ce1c21c957b12493223061",
-          "message": "Update Benchmark name",
-          "timestamp": "2024-08-27T19:27:07Z",
-          "url": "https://github.com/rebeckerspecialties/benchmarking-test-app/commit/3dbac4cc2c8f8a8906ce1c21c957b12493223061"
-        },
-        "date": 1724788479132,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "jsc/Apple iPhone 15 Pro/bitEcsBenchmarkWallClock-ms.txt",
-            "value": 4538,
-            "unit": "ms"
-          },
-          {
-            "name": "jsc/Apple iPhone 15 Pro/simpleBenchmarkWallClock-ms.txt",
-            "value": 5747,
-            "unit": "ms"
-          },
-          {
-            "name": "jsc/Apple iPhone 15 Pro/threeJsBenchmarkWallClock-ms.txt",
-            "value": 674,
-            "unit": "ms"
-          },
-          {
-            "name": "hermes/Apple iPhone 15 Pro/bitEcsBenchmarkWallClock-ms.txt",
-            "value": 4249,
-            "unit": "ms"
-          },
-          {
-            "name": "hermes/Apple iPhone 15 Pro/simpleBenchmarkWallClock-ms.txt",
-            "value": 5732,
-            "unit": "ms"
-          },
-          {
-            "name": "hermes/Apple iPhone 15 Pro/threeJsBenchmarkWallClock-ms.txt",
-            "value": 1182,
-            "unit": "ms"
-          }
-        ]
-      }
-    ]
-  }
-}
+    ],
+  },
+};
