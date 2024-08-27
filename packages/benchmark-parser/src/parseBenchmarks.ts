50acd858123dd352b788c7092fbcc26b496400d5
Henry Allen <hmallen99@gmail.com>
2024-08-27 10:10:36 -0400

    Create parseBenchmark file

--- packages/benchmark-parser/src/parseBenchmarks.ts
+++ packages/benchmark-parser/src/parseBenchmarks.ts
@@ -1,25 +1,43 @@
-import { open, Options, ZipFile } from "yauzl";
+import { readdir, readFile, writeFile } from "fs/promises";
 
-async function openZipFile(path: string, options: Options) {
-  return new Promise((resolve, reject) => {
-    open(path, options, (err, zipFile) => {
-      if (err) {
-        reject(err);
-        return;
-      }
+interface BenchmarkEntry {
+  name: string;
+  unit: string;
+  value: number;
+  range?: string;
+  extra?: string;
+}
 
-      resolve(zipFile);
+function createBenchmarkEntry(
+  file: string,
+  result: string,
+  unit: string
+): BenchmarkEntry {
+  return {
+    name: file,
+    unit,
+    value: parseInt(result),
+  };
+}
+
+async function parseBenchmarks() {
+  try {
+    const benchmarkEntries: BenchmarkEntry[] = [];
+
+    const files = await readdir("benchmarks", {
+      recursive: true,
     });
-  });
+
+    for (const file of files) {
+      const result = await readFile(file, "utf-8");
+      benchmarkEntries.push(createBenchmarkEntry(file, result, "ms"));
+    }
+
+    await writeFile("benchmark.json", JSON.stringify(benchmarkEntries));
+    console.log("Successfully wrote results to benchmark.json");
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-async function readZipEntry(zipFile: ZipFile) {
-  return new Promise((resolve) => {
-    zipFile.readEntry();
-    zipFile.on("entry", () => {});
-  });
-}
-
-function extractBenchmarkZips(paths: string[]) {}
-
-async function parseBenchmarks() {}
+parseBenchmarks();
