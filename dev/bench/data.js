4563bc9904cd98f23282a66c2c42a4f6c28f0f71
github-action-benchmark <github@users.noreply.github.com>
2024-08-27 19:09:47 +0000

    add React Native Benchmarks (customSmallerIsBetter) benchmark result for 0ded53cb0484e2b07013a1fc3c1f9b8e2950496b

---
+++ dev/bench/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1724785787048,
+  "repoUrl": "https://github.com/rebeckerspecialties/benchmarking-test-app",
+  "entries": {
+    "React Native Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "Henry Allen",
+            "username": "hmallen99",
+            "email": "hmallen99@gmail.com"
+          },
+          "committer": {
+            "name": "Henry Allen",
+            "username": "hmallen99",
+            "email": "hmallen99@gmail.com"
+          },
+          "id": "0ded53cb0484e2b07013a1fc3c1f9b8e2950496b",
+          "message": "Fix parameters",
+          "timestamp": "2024-08-27T18:42:26Z",
+          "url": "https://github.com/rebeckerspecialties/benchmarking-test-app/commit/0ded53cb0484e2b07013a1fc3c1f9b8e2950496b"
+        },
+        "date": 1724785786600,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "jsc/Apple iPhone 15 Pro/bitEcsBenchmark-clock.txt",
+            "value": 4443,
+            "unit": "clock"
+          },
+          {
+            "name": "jsc/Apple iPhone 15 Pro/simpleBenchmark-clock.txt",
+            "value": 5764,
+            "unit": "clock"
+          },
+          {
+            "name": "jsc/Apple iPhone 15 Pro/threeJsBenchmark-clock.txt",
+            "value": 688,
+            "unit": "clock"
+          },
+          {
+            "name": "hermes/Apple iPhone 15 Pro/bitEcsBenchmark-clock.txt",
+            "value": 4242,
+            "unit": "clock"
+          },
+          {
+            "name": "hermes/Apple iPhone 15 Pro/simpleBenchmark-clock.txt",
+            "value": 5732,
+            "unit": "clock"
+          },
+          {
+            "name": "hermes/Apple iPhone 15 Pro/threeJsBenchmark-clock.txt",
+            "value": 1214,
+            "unit": "clock"
+          }
+        ]
+      }
+    ]
+  }
+}
