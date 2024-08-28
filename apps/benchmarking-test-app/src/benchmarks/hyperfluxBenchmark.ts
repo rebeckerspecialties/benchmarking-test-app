355fd1db4dae9062c80947e4e7932585d17cdfb6
Henry Allen <hmallen99@gmail.com>
2024-08-28 14:51:07 -0400

    Fix HyperFlux benchmark

--- apps/benchmarking-test-app/src/benchmarks/hyperfluxBenchmark.ts
+++ apps/benchmarking-test-app/src/benchmarks/hyperfluxBenchmark.ts
@@ -1,3 +1,4 @@
+import "react-native-get-random-values";
 import {
   applyIncomingActions,
   createHyperStore,
@@ -10,47 +11,46 @@ import {
   matchesWithDefault,
 } from "@ir-engine/hyperflux";
 
-const ACTION_COUNT = 5000;
-const ITERATIONS = 5000;
+const ACTION_COUNT = 500;
+const ITERATIONS = 50;
 
 export const hyperfluxBenchmark = async () => {
-  for (let i = 0; i < ITERATIONS; i++) {
-    const greet = defineAction({
-      type: "TEST_GREETING",
-      greeting: matchesWithDefault(matches.string, () => "hi"),
-    });
-    const goodbye = defineAction({
-      type: "TEST_GOODBYE",
-      greeting: matchesWithDefault(matches.string, () => "bye"),
-    });
-    const HospitalityState = defineState({
-      name: "test.hospitality",
+  await new Promise((resolve) => setTimeout(resolve, 10));
+  const greet = defineAction({
+    type: "TEST_GREETING",
+    greeting: matchesWithDefault(matches.string, () => "hi"),
+  });
+  const goodbye = defineAction({
+    type: "TEST_GOODBYE",
+    greeting: matchesWithDefault(matches.string, () => "bye"),
+  });
+  const HospitalityState = defineState({
+    name: "test.hospitality",
 
-      initial: () => ({
-        greetingCount: 0,
-        firstGreeting: null as string | null,
+    initial: () => ({
+      greetingCount: 0,
+      firstGreeting: null as string | null,
+    }),
+
+    receptors: {
+      onGreet: greet.receive((action) => {
+        const state = getMutableState(HospitalityState);
+        state.greetingCount.set((v) => v + 1);
+        if (!state.firstGreeting.value)
+          state.firstGreeting.set(action.greeting);
       }),
-
-      receptors: {
-        onGreet: greet.receive((action) => {
-          const state = getMutableState(HospitalityState);
-          state.greetingCount.set((v) => v + 1);
-          if (!state.firstGreeting.value)
-            state.firstGreeting.set(action.greeting);
-        }),
-        onGoodbye: goodbye.receive((action) => {
-          const state = getMutableState(HospitalityState);
-          state.greetingCount.set((v) => v - 1);
-          if (!state.firstGreeting.value)
-            state.firstGreeting.set(action.greeting);
-        }),
-      },
-    });
-
-    createHyperStore({
-      getDispatchTime: () => Date.now(),
-    });
-
+      onGoodbye: goodbye.receive((action) => {
+        const state = getMutableState(HospitalityState);
+        state.greetingCount.set((v) => v - 1);
+        if (!state.firstGreeting.value)
+          state.firstGreeting.set(action.greeting);
+      }),
+    },
+  });
+  createHyperStore({
+    getDispatchTime: () => Date.now(),
+  });
+  for (let i = 0; i < ITERATIONS; i++) {
     const queue = defineActionQueue([greet.matches, goodbye.matches]);
     for (let j = 0; j < ACTION_COUNT; j++) {
       dispatchAction(greet({}));
