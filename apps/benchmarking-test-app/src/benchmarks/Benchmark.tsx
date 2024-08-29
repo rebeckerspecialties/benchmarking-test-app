e86c80b004ae38cdb2dd1771bbfb13a35aeaf607
Henry Allen <31718268+hmallen99@users.noreply.github.com>
2024-08-29 16:55:14 -0400

    Profile Hyperflux and IR ECS hot paths (#30)
    * Add Hyperflux benchmark
    
    * Register hyperflux benchmark
    
    * Add state to benchmark
    
    * Add ECS benchmark
    
    * register ECS benchmark
    
    * Fix HyperFlux benchmark
    
    * Add perf hooks override
    
    * Remove isClient log
    
    * Add more iterations
    
    * Add getComponent and getOptionalComponent

--- apps/benchmarking-test-app/src/benchmarks/Benchmark.tsx
+++ apps/benchmarking-test-app/src/benchmarks/Benchmark.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { Button, Text } from "react-native";
 
 export interface BenchmarkProps {
@@ -8,17 +8,23 @@ export interface BenchmarkProps {
 
 export const Benchmark: React.FC<BenchmarkProps> = ({ name, run }) => {
   const [benchmarkRunTime, setBenchmarkRunTime] = useState<number | null>(null);
-
+  const [error, setError] = useState<Error>();
   const [running, setRunning] = useState(false);
 
   const onBeginBenchmark = useCallback(async () => {
     setRunning(true);
     const startTime = Date.now();
-    await run();
+    await run().catch((err) => {
+      setError(err);
+    });
     const timeDelta = Date.now() - startTime;
     setRunning(false);
     setBenchmarkRunTime(timeDelta);
   }, [run]);
+
+  if (error) {
+    return <Text>{`${error}`}</Text>;
+  }
 
   if (running) {
     return <Text>Running benchmark, please wait</Text>;
---
+++ packages/perf_hooks/README.md
@@ -0,0 +1,1 @@
+Override package for node's perf_hooks module. This can be added to the `metro.config.js` file to satisfy `require("perf_hooks")` within javascript libraries.
