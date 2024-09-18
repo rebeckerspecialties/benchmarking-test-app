35d309ce4db6f726b2340a8e7aab8ef50d6f325f
Henry Allen <hmallen99@gmail.com>
2024-09-18 12:46:00 -0400

    Split out parsing step

--- packages/benchmark-parser/src/index.ts
+++ packages/benchmark-parser/src/index.ts
@@ -3,6 +3,6 @@ import path from "path";
 
 const benchmarkPath = path.resolve(process.cwd(), "benchmarks");
 const outputPath = path.resolve(process.cwd(), "benchmark.json");
-const bundlePath = path.resolve(process.cwd(), "main.ios.jsbundle");
+const bundlePath = path.resolve(process.cwd(), "bundle/main.ios.jsbundle");
 
 parseBenchmarks(benchmarkPath, bundlePath, outputPath);
