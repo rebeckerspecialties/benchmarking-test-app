2e5a3ae85b678b47afe20cf8f9b6ebd056d326cb
Henry Allen <hmallen99@gmail.com>
2024-10-03 17:19:26 -0400

    Prep ssgi_compose for conversion

--- apps/benchmarking-test-app/src/graphicsBenchmarks/shaders/ssgi_compose.frag.glsl
+++ apps/benchmarking-test-app/src/graphicsBenchmarks/shaders/ssgi_compose.frag.glsl
@@ -1,36 +1,20 @@
 // https://github.com/0beqz/realism-effects/blob/main/src/ssgi/shader/ssgi_compose.frag
-uniform sampler2D inputTexture;
-uniform sampler2D sceneTexture;
-uniform highp sampler2D depthTexture;
-uniform bool isDebug;
+layout(location = 0) in vec2 uv;
+layout(location = 0) out vec4 outputColor;
 
-uniform float cameraNear;
-uniform float cameraFar;
+layout(binding = 0) uniform texture2D inputTexture;
+layout(binding = 1) uniform texture2D sceneTexture;
+layout(binding = 2) uniform texture2D depthTexture;
+layout(binding = 3) uniform sampler samp;
 
-// source: https://github.com/mrdoob/three.js/blob/1e352b62a57f78a032019c37c8c33e3bf7839c04/src/renderers/shaders/ShaderChunk/packing.glsl.js#L97
-float perspectiveDepthToViewZ(const in float depth, const in float near, const in float far) {
-	// maps perspective depth in [ 0, 1 ] to viewZ
-  return (near * far) / ((far - near) * depth - far);
-}
-
-// source: https://github.com/mrdoob/three.js/blob/79ea10830dfc97b6c0a7e29d217c7ff04c081095/examples/jsm/shaders/BokehShader.js#L66
-float getViewZ(const in float depth) {
-  return perspectiveDepthToViewZ(depth, cameraNear, cameraFar);
-}
-
-void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
-  if(isDebug) {
-    outputColor = textureLod(inputTexture, uv, 0.);
-    return;
-  }
-
-  float depth = textureLod(depthTexture, uv, 0.).r;
+void main() {
+  float depth = textureLod(sampler2D(depthTexture, samp), uv, 0.).r;
   vec3 ssgiClr;
 
   if(depth == 1.0) {
-    ssgiClr = textureLod(sceneTexture, uv, 0.).rgb;
+    ssgiClr = textureLod(sampler2D(sceneTexture, samp), uv, 0.).rgb;
   } else {
-    ssgiClr = textureLod(inputTexture, uv, 0.).rgb;
+    ssgiClr = textureLod(sampler2D(inputTexture, samp), uv, 0.).rgb;
   }
 
   outputColor = vec4(ssgiClr, 1.0);
