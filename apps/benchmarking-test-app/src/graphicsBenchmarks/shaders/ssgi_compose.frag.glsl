aa6cf31723c3c0b50cfd04ca5c27f1bfb8cc1139
Henry Allen <hmallen99@gmail.com>
2024-10-03 15:22:07 -0400

    Clean up compose frag

--- apps/benchmarking-test-app/src/graphicsBenchmarks/shaders/ssgi_compose.frag.glsl
+++ apps/benchmarking-test-app/src/graphicsBenchmarks/shaders/ssgi_compose.frag.glsl
@@ -7,15 +7,15 @@ uniform bool isDebug;
 uniform float cameraNear;
 uniform float cameraFar;
 
-#include <fog_pars_fragment>
+// source: https://github.com/mrdoob/three.js/blob/1e352b62a57f78a032019c37c8c33e3bf7839c04/src/renderers/shaders/ShaderChunk/packing.glsl.js#L97
+float perspectiveDepthToViewZ(const in float depth, const in float near, const in float far) {
+	// maps perspective depth in [ 0, 1 ] to viewZ
+  return (near * far) / ((far - near) * depth - far);
+}
 
 // source: https://github.com/mrdoob/three.js/blob/79ea10830dfc97b6c0a7e29d217c7ff04c081095/examples/jsm/shaders/BokehShader.js#L66
 float getViewZ(const in float depth) {
-#if PERSPECTIVE_CAMERA == 1
   return perspectiveDepthToViewZ(depth, cameraNear, cameraFar);
-#else
-  return orthographicDepthToViewZ(depth, cameraNear, cameraFar);
-#endif
 }
 
 void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
@@ -31,15 +31,6 @@ void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
     ssgiClr = textureLod(sceneTexture, uv, 0.).rgb;
   } else {
     ssgiClr = textureLod(inputTexture, uv, 0.).rgb;
-
-#ifdef USE_FOG
-    float viewZ = getViewZ(depth) * 0.4; // todo: find why 0.4 is needed to somewhat match three.js's result
-    vFogDepth = -viewZ;
-
-#include <fog_fragment>
-
-    ssgiClr = mix(ssgiClr, fogColor, fogFactor);
-#endif
   }
 
   outputColor = vec4(ssgiClr, 1.0);
