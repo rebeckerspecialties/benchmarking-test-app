275f7a00fcbb3229ab91132616207eab8587a9c4
Henry Allen <hmallen99@gmail.com>
2024-10-07 16:33:16 -0400

    Update benchmark path

--- packages/benchmark-parser/src/index.ts
+++ packages/benchmark-parser/src/index.ts
@@ -5,7 +5,7 @@ const pathToBenchmarks = path.resolve(process.cwd(), "benchmarks");
 const outputPath = path.resolve(process.cwd(), "benchmark.json");
 const bundlePath = path.resolve(process.cwd(), "bundleDist/main.ios.jsbundle");
 const executablePath = path.resolve(process.cwd(), "jscBenchmarking.ipa");
-const xmlPath = path.resolve(process.cwd(), "sdfBenchmark.xml");
+const xmlPath = path.resolve(pathToBenchmarks, "sdfWebGpuBenchmarkTrace.xml");
 
 parseBenchmarks({
   pathToBenchmarks,
